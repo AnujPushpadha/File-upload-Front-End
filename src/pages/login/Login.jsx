280ea9002b4e53f4ae04a0c4892d753348df0a63
Anuj <pushpadhaanuj@gmail.com>
2023-03-27 20:07:13 +0530

    update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import "./style.css";
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
